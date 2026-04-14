b1077ca81fd154216a13a6f7181b908faf55640d
Neji2615 <101935172+Neji2615@users.noreply.github.com>
2026-04-14 15:09:34 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        {showSplash && <SplashScreen onFinish={handleSplashFinish} />}
+        {/* {showSplash && <SplashScreen onFinish={handleSplashFinish} />} */}
         <BrowserRouter>
           <AuthProvider>
             <Routes>
